e7204dfc90971d6c2ff1aeaaa629eab135c00bb6
Shozzy1 <shozzy95@gmail.com>
2017-07-19 16:48:42 +0300

    Update view.php

--- modules/projects/view.php
+++ modules/projects/view.php
@@ -5,7 +5,7 @@
 	$seven_days = date("Y-m-d",mktime(0, 0, 0, date("m")  , date("d")+6, date("Y")));  
 	
 	///////////////////////////////////
-	$sql_show = "SELECT * FROM projects";
+	$sql_show = "SELECT c.id, c.title, c.color, COUNT(*) AS taskCount  FROM projects AS c INNER JOIN tasks AS b ON b.project_id = c.id GROUP BY c.id";
     $res = mysqli_query($conn, $sql_show);
     ///////////////////////////////////
    
