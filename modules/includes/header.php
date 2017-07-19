497e1d70b44e791ac6738117bd505313ecb9c4c5
Shozzy1 <shozzy95@gmail.com>
2017-07-19 16:50:43 +0300

    Update header.php

--- modules/includes/header.php
+++ modules/includes/header.php
@@ -77,7 +77,9 @@ require('/modules/projects/view.php');
 <div class="col-md-6 proj">
 <p class="circle" style="background: <?=$result['color']?>;"></p>
 <a href="index.php?mod=sort_project?id=<?=$result['id'];?>"><?php echo $result['title'];?></a>
-<?php echo $result['taskCounter'];?>
+<div style="float: right;">
+	<?php echo $result['taskCount'];?>
+</div>
 </div>
 <?php if (!isset($_SESSION['login']) && empty($_SESSION['login'])) {
  } else {?>
